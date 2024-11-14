4e72ce7e20ab6492afce7f4697429e9c741d14d7
eminorhan <aeminorhan@gmail.com>
2024-11-14 16:33:54 -0500

    add h1 code

--- data/willett/create_willett.py
+++ data/willett/create_willett.py
@@ -2,11 +2,12 @@ import os
 import argparse
 import numpy as np
 from scipy.io import loadmat
+from datasets import Dataset
 
 def get_args_parser():
     parser = argparse.ArgumentParser('Consolidate data in multiple files into a single file', add_help=False)
-    parser.add_argument('--save_str', default='train', type=str, help='String identifier for file to be saved')
-    parser.add_argument('--data_dir', default='train', type=str, help='Data directory')
+    parser.add_argument('--save_str', default='test', type=str, help='String identifier for file to be saved')
+    parser.add_argument('--data_dir', default='test', type=str, help='Data directory')
     return parser
 
 if __name__ == '__main__':
@@ -20,11 +21,30 @@ if __name__ == '__main__':
 
     # this dict is for registering the recording day across data set splits
     files_dict_for_day_id = {
-        't12.2022.04.28.mat': 0, 't12.2022.05.05.mat': 1, 't12.2022.05.17.mat': 2, 't12.2022.05.19.mat': 3, 't12.2022.05.24.mat': 4, 
-        't12.2022.05.26.mat': 5, 't12.2022.06.02.mat': 6, 't12.2022.06.07.mat': 7, 't12.2022.06.14.mat': 8, 't12.2022.06.16.mat': 9, 
-        't12.2022.06.21.mat': 10, 't12.2022.06.23.mat': 11, 't12.2022.06.28.mat': 12, 't12.2022.07.05.mat': 13, 't12.2022.07.14.mat': 14, 
-        't12.2022.07.21.mat': 15, 't12.2022.07.27.mat': 16, 't12.2022.07.29.mat': 17, 't12.2022.08.02.mat': 18, 't12.2022.08.11.mat': 19, 
-        't12.2022.08.13.mat': 20, 't12.2022.08.18.mat': 21, 't12.2022.08.23.mat': 22, 't12.2022.08.25.mat': 23
+        't12.2022.04.28.mat': 0, 
+        't12.2022.05.05.mat': 1, 
+        't12.2022.05.17.mat': 2, 
+        't12.2022.05.19.mat': 3, 
+        't12.2022.05.24.mat': 4, 
+        't12.2022.05.26.mat': 5, 
+        't12.2022.06.02.mat': 6, 
+        't12.2022.06.07.mat': 7, 
+        't12.2022.06.14.mat': 8, 
+        't12.2022.06.16.mat': 9, 
+        't12.2022.06.21.mat': 10, 
+        't12.2022.06.23.mat': 11, 
+        't12.2022.06.28.mat': 12, 
+        't12.2022.07.05.mat': 13, 
+        't12.2022.07.14.mat': 14, 
+        't12.2022.07.21.mat': 15, 
+        't12.2022.07.27.mat': 16, 
+        't12.2022.07.29.mat': 17, 
+        't12.2022.08.02.mat': 18, 
+        't12.2022.08.11.mat': 19, 
+        't12.2022.08.13.mat': 20, 
+        't12.2022.08.18.mat': 21, 
+        't12.2022.08.23.mat': 22, 
+        't12.2022.08.25.mat': 23
         }
     
     file_idx = 0
@@ -54,13 +74,21 @@ if __name__ == '__main__':
     block_id = np.concatenate(block_id)
     day_id = np.concatenate(day_id)
 
-    # save all data as a single .npz file
-    np.savez(f"{args.save_str}.npz", 
-            sentence=sentence,
-            spike_power=spike_power,
-            tx1=tx1,
-            tx2=tx2,
-            tx3=tx3,
-            tx4=tx4,
-            block_id=block_id,
-            day_id=day_id)
+    def gen_data():
+        for a, b, c, d, e, f, g, h in zip(sentence, spike_power, tx1, tx2, tx3, tx4, block_id, day_id):
+            yield {
+                "sentence": a,
+                "spike_power": b,
+                "tx1": c,
+                "tx2": d,
+                "tx3": e,
+                "tx4": f,
+                "block_id": g,
+                "day_id": h
+                }
+
+    ds = Dataset.from_generator(gen_data)
+        
+    # push all data to hub under "all"
+    ds.push_to_hub("eminorhan/willett", split=args.save_str, token=True)
+
